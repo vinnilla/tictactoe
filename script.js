efd1be2369265b90a0891b8460b866f6f71b6f6d
Vincent Huang <vincentckhuang@gmail.com>
2016-06-24 19:59:04 -0700

    added functionality to reset button

--- script.js
+++ script.js
@@ -10,8 +10,6 @@ var player=1;
 
 //store pointers to each main div in an array
 var sectionArray = document.getElementsByClassName('main');
-// //store pointer to each center img in an array
-// var centerArray = document.getElementsByClassName('center');
 //initiate for loop to populate each div with an eventlistener
 for (var i=0; i<sectionArray.length; i++) {
 	sectionArray[i].addEventListener('click', function() {
@@ -28,4 +26,15 @@ for (var i=0; i<sectionArray.length; i++) {
 	});
 }
 
-
+//make reset button work
+var button = document.querySelector('button');
+button.addEventListener('click', function() {
+	for (var i=0; i<sectionArray.length; i++) {
+		if (sectionArray[i].firstChild.classList.contains('cross')){
+			sectionArray[i].firstChild.classList.remove('cross');
+		}
+		else {
+			sectionArray[i].firstChild.classList.remove('circle');	
+		}	
+	}
+});
