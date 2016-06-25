95e1d136b964bae0af4a3c1fd0ed029a9fd66994
Vincent Huang <vincentckhuang@gmail.com>
2016-06-24 20:17:40 -0700

    limited play on each cell to only once

--- script.js
+++ script.js
@@ -7,29 +7,42 @@ var board = [
 //create global variable to keep track of which turn it is
 //this will also act as a counter
 var player=1;
-
 //store pointers to each main div in an array
 var sectionArray = document.getElementsByClassName('main');
-//initiate for loop to populate each div with an eventlistener
-for (var i=0; i<sectionArray.length; i++) {
-	sectionArray[i].addEventListener('click', function() {
-
-		if (player%2 != 0) { //player 1
-			// centerArray[i].classList.add('cross');
+//create function to add appropriate classes
+var addClass = function() {
+	if (player%2 != 0) { //check if player 1
+			//add .cross to the div found within
 			this.firstChild.classList.add('cross');
 		}
 		else { //player 2
-			// centerArray[i].classList.add('circle')
+			//add .circle to the div found within
 			this.firstChild.classList.add('circle');
 		}
+		//add to counter which creates the alternating turn functionality
 		player++;
+		//FOR TESTING
+		//console.log(player);
+}
+
+//initiate for loop to populate each div with an eventlistener
+for (var i=0; i<sectionArray.length; i++) {
+	//addEventListener
+	sectionArray[i].addEventListener('mousedown', addClass);
+	//remove previously added event listener to disable clicking twice
+	sectionArray[i].addEventListener('mouseup', function() {
+		this.removeEventListener('mousedown', addClass);
 	});
 }
 
 //make reset button work
+//store pointer to the button
 var button = document.querySelector('button');
+//addEventListener
 button.addEventListener('click', function() {
+	//iterate through the number of divs (9)
 	for (var i=0; i<sectionArray.length; i++) {
+		//check if class cross exists
 		if (sectionArray[i].firstChild.classList.contains('cross')){
 			sectionArray[i].firstChild.classList.remove('cross');
 		}
